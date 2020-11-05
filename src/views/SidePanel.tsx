9955c66a748e00dd9b14efe3c2296c9c757ada3c
Dacia Rodrigue <msdessin@gmail.com>
2020-11-05 09:17:05 -0600

    styling

--- src/views/SidePanel.tsx
+++ src/views/SidePanel.tsx
@@ -1,3 +1,5 @@
+import { percent, rem } from 'csx'
+import { style } from 'typestyle'
 import { component } from 'vue-tsx-support'
 
 import noteStore from '@/store/noteStore'
@@ -6,9 +8,13 @@ export default component({
 	name: 'SidePanel',
 	render() {
 		return (
-			<div>
+			<div class={noteStore.isSidePanelOpen ? styleSidePanel : ''}>
 				{!noteStore.isSidePanelOpen &&
-					<p onClick={() => noteStore.showSidePanel()} data-test="add-personal-note">Add New Note</p>
+					<p
+						class="add-new-note enhanced-style"
+						onClick={() => noteStore.showSidePanel()}
+						data-test="add-personal-note"
+					>Add New Note</p>
 				}
 
 				{this.$slots.default}
@@ -16,3 +22,13 @@ export default component({
 		)
 	},
 })
+
+const styleSidePanel = style(
+	{
+		alignItems: 'center',
+		display: 'flex',
+		justifyContent: 'center',
+		padding: rem(1),
+		width: percent(100),
+	},
+)
