c64d28b219e875a6bb00d739b6d234f8e4dded2d
Dacia Rodrigue <msdessin@gmail.com>
2020-11-04 21:38:14 -0600

    add and update component

---
+++ src/components/AddNote.tsx
@@ -0,0 +1,15 @@
+import { component } from 'vue-tsx-support'
+
+export default component({
+	name: 'AddNote',
+	render() {
+		return (
+			<section>
+				<h3>Add New Note</h3>
+				<p>Add a new note to your personal notes.</p>
+
+				{/* Not form goes here */}
+			</section>
+		)
+	},
+})
---
+++ src/components/UpdateNote.tsx
@@ -0,0 +1,14 @@
+import { component } from 'vue-tsx-support'
+
+export default component({
+	name: 'UpdateNote',
+	render() {
+		return (
+			<section>
+				<h3>Update Note</h3>
+
+				{/* Not form goes here */}
+			</section>
+		)
+	},
+})
